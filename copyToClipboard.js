5d3c382d26a188ac10a85b7693e037345d333a06
Harsh Arora <harsh.arora1821@gmail.com>
2019-12-06 10:07:44 +0800

    Update copyToClipboard.js

--- copyToClipboard.js
+++ copyToClipboard.js
@@ -1,7 +1,7 @@
 function copyToClipboard(elementID){
 	var aux = document.createElement("input");
 	aux.setAttribute('Value',document.getElementById(elementID).innerHTML);
- document.body.appendChild(aux);
+	document.body.appendChild(aux);
 	aux.select();
 	document.execCommand("copy");
 	document.body.removeChild(aux);
